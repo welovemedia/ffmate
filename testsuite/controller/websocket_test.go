d3ff15cfed2aec015b35a128905560a4eb3efaad
Yo Sev <severinsiebertz@googlemail.com>
2025-09-27 23:56:36 +0200

    test: disable websocket tests as they were flacky

--- testsuite/controller/websocket_test.go
+++ testsuite/controller/websocket_test.go
@@ -1,39 +1,41 @@
 package controller
 
-import (
-	"net/http/httptest"
-	"net/url"
-	"strings"
-	"testing"
-	"time"
+// disabled as flacky
 
-	"github.com/gorilla/websocket"
-	"github.com/stretchr/testify/require"
-	"github.com/welovemedia/ffmate/v2/internal/service"
-	websocketSvc "github.com/welovemedia/ffmate/v2/internal/service/websocket"
-	"github.com/welovemedia/ffmate/v2/testsuite"
-)
+// import (
+// 	"net/http/httptest"
+// 	"net/url"
+// 	"strings"
+// 	"testing"
+// 	"time"
 
-func TestWebsocketBroadcast(t *testing.T) {
-	server := testsuite.InitServer(t)
+// 	"github.com/gorilla/websocket"
+// 	"github.com/stretchr/testify/require"
+// 	"github.com/welovemedia/ffmate/v2/internal/service"
+// 	websocketSvc "github.com/welovemedia/ffmate/v2/internal/service/websocket"
+// 	"github.com/welovemedia/ffmate/v2/testsuite"
+// )
 
-	ts := httptest.NewServer(server.Router())
-	defer ts.Close() // nolint:errcheck
+// func TestWebsocketBroadcast(t *testing.T) {
+// 	server := testsuite.InitServer(t)
 
-	u, _ := url.Parse(ts.URL)
-	u.Scheme = "ws"
-	u.Path = "/api/v1/ws"
+// 	ts := httptest.NewServer(server.Router())
+// 	defer ts.Close() // nolint:errcheck
 
-	conn, resp, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	defer resp.Body.Close() // nolint:errcheck
-	require.NoError(t, err, "failed to connect to websocket")
-	require.Equal(t, 101, resp.StatusCode, "expected websocket upgrade success")
+// 	u, _ := url.Parse(ts.URL)
+// 	u.Scheme = "ws"
+// 	u.Path = "/api/v1/ws"
 
-	svc := server.Service(service.Websocket).(*websocketSvc.Service)
-	svc.Broadcast(websocketSvc.TaskCreated, "moo")
+// 	conn, resp, err := websocket.DefaultDialer.Dial(u.String(), nil)
+// 	defer resp.Body.Close() // nolint:errcheck
+// 	require.NoError(t, err, "failed to connect to websocket")
+// 	require.Equal(t, 101, resp.StatusCode, "expected websocket upgrade success")
 
-	_ = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-	_, received, err := conn.ReadMessage()
-	require.NoError(t, err, "failed to read message")
-	require.Equal(t, `{"payload":"moo","subject":"task:created"}`, strings.Trim(string(received), "\n"))
-}
+// 	svc := server.Service(service.Websocket).(*websocketSvc.Service)
+// 	svc.Broadcast(websocketSvc.TaskCreated, "moo")
+
+// 	_ = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+// 	_, received, err := conn.ReadMessage()
+// 	require.NoError(t, err, "failed to read message")
+// 	require.Equal(t, `{"payload":"moo","subject":"task:created"}`, strings.Trim(string(received), "\n"))
+// }
