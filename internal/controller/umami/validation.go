9b74c3b4743ed0e03ad3b5a58c7f6a42fe3ad3dc
Yo Sev <severinsiebertz@googlemail.com>
2025-09-27 23:51:33 +0200

    test(umami): change validator to use payload as prefix

--- internal/controller/umami/validation.go
+++ internal/controller/umami/validation.go
@@ -8,12 +8,12 @@ import (
 func (c *Controller) UmamiRequest(_ *goyave.Request) v.RuleSet {
 	return v.RuleSet{
 		{Path: v.CurrentElement, Rules: v.List{v.Object()}},
-		{Path: "hostname", Rules: v.List{v.String(), v.Required()}},
-		{Path: "language", Rules: v.List{v.String(), v.Required()}},
-		{Path: "referrer", Rules: v.List{v.String(), v.Required()}},
-		{Path: "screen", Rules: v.List{v.String(), v.Required()}},
-		{Path: "title", Rules: v.List{v.String(), v.Required()}},
-		{Path: "url", Rules: v.List{v.String(), v.Required()}},
-		{Path: "website", Rules: v.List{v.String(), v.Required()}},
+		{Path: "payload.hostname", Rules: v.List{v.String(), v.Required()}},
+		{Path: "payload.language", Rules: v.List{v.String(), v.Required()}},
+		{Path: "payload.referrer", Rules: v.List{v.String(), v.Required()}},
+		{Path: "payload.screen", Rules: v.List{v.String(), v.Required()}},
+		{Path: "payload.title", Rules: v.List{v.String(), v.Required()}},
+		{Path: "payload.url", Rules: v.List{v.String(), v.Required()}},
+		{Path: "payload.website", Rules: v.List{v.String(), v.Required()}},
 	}
 }
