6885ee3e7d3d072a33808ec3d6b2d932749070ff
YoSev <severinsiebertz@googlemail.com>
2026-08-09 13:29:17 +0200

    fix: various bugs regarding the new labels (ai)

--- testsuite/controller/debug_test.go
+++ testsuite/controller/debug_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/welovemedia/ffmate/v2/testsuite"
 
-	_ "goyave.dev/goyave/v5/database/dialect/sqlite"
+	_ "github.com/welovemedia/ffmate/v2/internal/dialect"
 )
 
 func TestDebugSet(t *testing.T) {
